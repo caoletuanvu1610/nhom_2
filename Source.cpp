b554360a1b4917b8474b2c080939f354383f247d
Cao Le Tuan Vu <caoletuanvu1610@gmail.com>
2020-10-16 10:59:03 +0700

    tinh tich

--- Source.cpp
+++ Source.cpp
@@ -5,10 +5,28 @@ int tong(int a, int b);
 int hieu(int a, int b);
 int tich(int a, int b);
 float thuong(int a, int b);
+int tich(int a, int b);
+
+int tich(int a, int b)
+{
+	return a * b;
+
+}
 int main()
 {
 	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
 
+	cout << "Nhap vao so a:";
+	cin >> a;
+	cout << "Nhap vao so b:";
+	cin >> b;
+	cout << "Tong 2 so " << a << " va " << b << " la: " << tong(a, b);
+	cout << "Hieu 2 so " << a << " va " << b << " la: " << hieu(a, b);
+	cout << "Tich 2 so " << a << " va " << b << " la: " << tich(a, b);
+	cout << "Thuong 2 so " << a << " va " << b << " la: " << thuong(a, b);
+
+
+	
 	system("pause");
 	return 0;
 }
