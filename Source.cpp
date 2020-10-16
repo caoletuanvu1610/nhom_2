e9f303b9c75396acf3b1b1786e2b430dee874b28
Truong Ngoc Khanh <khanhert@gmail.com>
2020-10-16 10:53:28 +0700

    phep chia

--- Source.cpp
+++ Source.cpp
@@ -5,6 +5,10 @@ int tong(int a, int b);
 int hieu(int a, int b);
 int tich(int a, int b);
 float thuong(int a, int b);
+float thuong(int a, int b) {
+	return(a / b);
+}
+
 int main()
 {
 	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
