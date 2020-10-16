8c60a86a1781520940ba1bff4a55021d33fb8540
unknown <you@example.com>
2020-10-16 10:59:15 +0700

    tinh tong

--- Source.cpp
+++ Source.cpp
@@ -5,10 +5,16 @@ int tong(int a, int b);
 int hieu(int a, int b);
 int tich(int a, int b);
 float thuong(int a, int b);
+int tong(int a, int b)
+{
+	int s = 0;
+	s = a + b;
+	return s;
+}
 int main()
 {
 	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
-
+	cout << "\nTong la:" << tong;
 	system("pause");
 	return 0;
 }
