f061b42dae35b065e9b293432a737185f497330d
caoletuanvu <caoletuanvu1610@gmail.com>
2020-10-23 10:13:25 +0700

    Tinh Tich

--- Source.cpp
+++ Source.cpp
@@ -10,31 +10,16 @@ int hieu(int a, int b) {
 	return hieu;
 }
 
-int tong(int a, int b);
-int hieu(int a, int b);
-int tich(int a, int b);
-float thuong(int a, int b);
-
 
 int tich(int a, int b);
 
+//TinhTich
 int tich(int a, int b)
 {
 	return a * b;
 
 }
-//Hieu
 
-int tong(int a, int b)
-{
-	int s = 0;
-	s = a + b;
-	return s;
-}
-
-float thuong(int a, int b) {
-	return(a / b);
-}
 
 int main()
 {
@@ -49,13 +34,9 @@ int main()
 	cout << "Nhap vao so b:";
 	cin >> b;
 
-	cout << "Hieu 2 so " << a << " va " << b << " la: " << hieu(a, b);
-	
 
-	cout << "Tong 2 so " << a << " va " << b << " la: " << tong(a, b);
-	cout << "Hieu 2 so " << a << " va " << b << " la: " << hieu(a, b);
 	cout << "Tich 2 so " << a << " va " << b << " la: " << tich(a, b);
-	cout << "Thuong 2 so " << a << " va " << b << " la: " << thuong(a, b);
+
 
 
 	
